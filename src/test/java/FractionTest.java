1cca7db6cd1b091759d2190f34228aee1c1d40bf
Bas Dijkstra <bas@ontestautomation.com>
2022-05-24 13:02:52 +0200

    Refactor: data driven test

--- src/test/java/FractionTest.java
+++ src/test/java/FractionTest.java
@@ -1,5 +1,7 @@
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 public class FractionTest {
 
@@ -11,15 +13,34 @@ public class FractionTest {
         });
     }
 
-    @Test
-    public void addFractions_sameDenominator_noSimplification() {
+    @ParameterizedTest
+    @CsvSource({
+            "1, 5, 2, 5, 3, 5",
+            "-1, 5, -2, 5, -3, 5"
+    })
+    public void addFractions_sameDenominator_noSimplification(
+            int numeratorOne, int denominatorOne, int numeratorTwo,
+            int denominatorTwo, int numeratorResult, int denominatorResult
+    ) {
 
-        Fraction fraction1 = new Fraction(1, 5);
-        Fraction fraction2 = new Fraction(2, 5);
+        Fraction fraction1 = new Fraction(numeratorOne, denominatorOne);
+        Fraction fraction2 = new Fraction(numeratorTwo, denominatorTwo);
 
         Fraction resultFraction = fraction1.add(fraction2);
 
-        Assertions.assertEquals(3, resultFraction.getNumerator());
+        Assertions.assertEquals(numeratorResult, resultFraction.getNumerator());
+        Assertions.assertEquals(denominatorResult, resultFraction.getDenominator());
+    }
+
+    @Test
+    public void addFractions_negativeNumerators_noSimplification() {
+
+        Fraction fraction1 = new Fraction(-1, 5);
+        Fraction fraction2 = new Fraction(-2, 5);
+
+        Fraction resultFraction = fraction1.add(fraction2);
+
+        Assertions.assertEquals(-3, resultFraction.getNumerator());
         Assertions.assertEquals(5, resultFraction.getDenominator());
     }
 }

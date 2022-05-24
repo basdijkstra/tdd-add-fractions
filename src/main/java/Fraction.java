bc790d369594a2c793d1fd429dc8b25fbe99756c
Bas Dijkstra <bas@ontestautomation.com>
2022-05-24 12:49:21 +0200

    Add support for 1/5 + 2/5 = 3/5

--- src/main/java/Fraction.java
+++ src/main/java/Fraction.java
@@ -6,10 +6,26 @@ public class Fraction {
     public Fraction(int numerator, int denominator) {
 
         this.numerator = numerator;
-        
+
         if (denominator == 0) {
             throw new IllegalArgumentException("Denominator cannot be zero");
         }
         this.denominator = denominator;
+    }
+
+    public int getNumerator() {
+        return numerator;
+    }
+
+    public int getDenominator() {
+        return denominator;
+    }
+
+    public Fraction add(Fraction fractionToAdd) {
+
+        return new Fraction(
+               this.numerator + fractionToAdd.getNumerator(),
+               this.denominator
+        );
     }
 }
--- src/test/java/FractionTest.java
+++ src/test/java/FractionTest.java
@@ -10,4 +10,16 @@ public class FractionTest {
             Fraction fraction = new Fraction(2, 0);
         });
     }
+
+    @Test
+    public void addFractions_sameDenominator_noSimplification() {
+
+        Fraction fraction1 = new Fraction(1, 5);
+        Fraction fraction2 = new Fraction(2, 5);
+
+        Fraction resultFraction = fraction1.add(fraction2);
+
+        Assertions.assertEquals(3, resultFraction.getNumerator());
+        Assertions.assertEquals(5, resultFraction.getDenominator());
+    }
 }
